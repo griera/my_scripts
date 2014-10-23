06d22e88e37e75e72c6333f43b88a4da79164ba6
griera <genis.riera.perez@gmail.com>
2014-10-23 17:26:24 +0200

    Display all 256 colors available on some terminals and terminal emulators like XTerm and GNOME Terminal

--- my_show_color_codes.sh
+++ my_show_color_codes.sh
@@ -3,10 +3,11 @@
 #
 # my_show_color_codes.sh:
 #
-# Echoes a bunch of color codes to the terminal to demonstrate what's
-# available. Each line is the color code of one foreground color, out of 17
-# (default + 16 escapes), followed by a test use of that color on all nine
-# background colors (default + 8 escapes).
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.#
 #
 # Exit status:
 #  · 0 -> Success
@@ -14,24 +15,18 @@
 # The text used to test color configuration
 TEXT_TEST='Test'
 
-echo -e "\n                   40m      41m      42m      43m\
-      44m      45m      46m      47m";
+#Foreground/Background
+for fgbg in 38 48 ; do
+    for color in {0..256} ; do
 
-for set_foregrounds in  '    m' '   1m' '   2m' '  30m' '1;30m' '2;30m' \
-                        '  31m' '1;31m' '2;31m' '  32m' '1;32m' '2;33m' \
-                        '  33m' '1;33m' '2;33m' '  34m' '1;34m' '2;34m' \
-                        '  35m' '1;35m' '2;35m' '  36m' '1;36m' '2;36m' \
-                        '  37m' '1;37m' '2;37m' ;
-do 
-    foreground=${set_foregrounds// /}
-    echo -en " $set_foregrounds \033[$foreground  $TEXT_TEST  "
+        echo -en "\e[${fgbg};5;${color}m ${TEXT_TEST}\t\e[0m"
 
-    for background in 40m 41m 42m 43m 44m 45m 46m 47m ;
-    do
-        echo -en "$EINS \033[$foreground\033[$background  $TEXT_TEST  \033[0m"
+        #Display 10 colors per lines
+        if [ $((($color + 1) % 10)) == 0 ] ; then
+            echo
+        fi
     done
     echo
 done
-echo
 
 exit 0
